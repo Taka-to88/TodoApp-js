561365309bb23343bf9fd91a6b985cdfef3f6bbf
TAKATO KONDO <75786773+Taka-to88@users.noreply.github.com>
2021-12-04 11:00:10 +0900

    JS追記 タスクの追加機能（テキスト）

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import "./styles.css";
+//テキストボックスの値を取得し、空にする
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-const onClickadd = () => {
-  alert();
+  //divを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
   .getElementById("add-button")
-  .addEventListener("click", () => onClickadd());
+  .addEventListener("click", () => onClickAdd());
