8d45b385bf7664882cfb5cbc3203242bca34751c
TAKATO KONDO <75786773+Taka-to88@users.noreply.github.com>
2021-12-04 13:32:49 +0900

    js追記 削除ボタン実装

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+
 //テキストボックスの値を取得し、空にする
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
@@ -12,9 +13,26 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button(完了)タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンのdivタグを未完了リストから削除
+    const deleteTaret = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTaret);
+  });
+
   //divタグの子要素に各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
